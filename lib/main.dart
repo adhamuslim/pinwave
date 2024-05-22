c83add9d435405ff141afd917ec8bd189eae7689
adhamuslim <121708479+adhamuslim@users.noreply.github.com>
2024-05-22 17:38:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
 import 'signup.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(PinWave());
 }
 
-class MyApp extends StatelessWidget {
+class PinWave extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SignInScreen(),
     );
   }
@@ -18,106 +20,131 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.grey[300], // Background color of the entire screen
-        child: Center(
+      backgroundColor: Colors.blue,
+      body: SingleChildScrollView(
+        child: SafeArea(
           child: Container(
-            decoration: BoxDecoration(
-              color: Colors.blue, // Light blue background color of the card
-              
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.waves,
-                    color: Colors.blue,
-                    size: 50,
-                  ),
+            color: Colors.grey[300], // Background color of the entire screen
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue, // Light blue background color of the card
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'Welcome To',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.indigo,
-                  ),
-                ),
-                Text(
-                  'PINWAVE',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
-                  ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  'You See What I See\n',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
-                  ),
-                ),
-                SizedBox(height: 30),
-                Row(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => signup()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.indigo,
-                        foregroundColor: Colors.white, // Button color
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            bottomLeft: Radius.circular(20),
-                          ),
-                        ),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 15,
-                          horizontal: 30,
-                        ),
+                    SizedBox(height: 200),
+                    CircleAvatar(
+                      radius: 75,
+                      backgroundColor: Colors.white,
+                      child: Image.asset(
+                        'assets/logo.jpeg', // Path ke gambar aset
+                        width: 100,
+                        height: 100,
                       ),
-                      child: Text('Sign Up'),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginPage()),
-                        );// Sign In button action
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white, // Button color
-                        foregroundColor: (Colors.indigo), // Text color
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(20),
-                            bottomRight: Radius.circular(20),
-                          ),
-                        ),
-                        side: BorderSide(color: Colors.indigo),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 15,
-                          horizontal: 30,
+                    SizedBox(height: 100),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 30),
+                      width: 550,
+                      height: 490,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(35),
+                          topRight: Radius.circular(35),
                         ),
                       ),
-                      child: Text('Sign In'),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Welcome To',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.indigo,
+                            ),
+                          ),
+                          Text(
+                            'PINWAVE',
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'You See What I See\n',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 50),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => signup(),
+                                    ),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  foregroundColor: Colors.white, // Button color
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(20),
+                                      bottomLeft: Radius.circular(20),
+                                    ),
+                                  ),
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: 20,
+                                    horizontal: 30,
+                                  ),
+                                ),
+                                child: Text('Sign Up'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => LoginPage(),
+                                    ),
+                                  ); // Sign In button action
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white, // Button color
+                                  foregroundColor: Colors.blue, // Text color
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(20),
+                                      bottomRight: Radius.circular(20),
+                                    ),
+                                  ),
+                                  side: BorderSide(color: Colors.blue),
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: 20,
+                                    horizontal: 30,
+                                  ),
+                                ),
+                                child: Text('Sign In'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
